ed3e632ea1edb510d0488bdf4cdca4be3a2fa9b2
vegita24 <sahil242saini@gmail.com>
2024-05-12 16:03:27 +0530

    Diamond Prob Resolved via interface

--- DiamondProb/src/DiamondProb/InterfaceImp.java
+++ DiamondProb/src/DiamondProb/InterfaceImp.java
@@ -1,8 +1,7 @@
 package DiamondProb;
 
 public class InterfaceImp implements ChildA, ChildB {
-    // giving exception here, must have to implement foo() here
-    @Override
+    // giving exception here, must have to implement foo() here (solution to diamond prob)
     public void foo() {
         System.out.println("must implement & override foo here");
     }

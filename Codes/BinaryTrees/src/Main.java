7293913b441bd94f9b400e736d5d12c603f07c97
vegita24 <sahil242saini@gmail.com>
2024-07-03 21:26:56 +0530

    refactored code

--- Codes/BinaryTrees/src/Main.java
+++ Codes/BinaryTrees/src/Main.java
@@ -1,4 +1,5 @@
 import topView.BinaryTree;
+import topView.Pair;
 import topView.TopView;
 
 import java.util.TreeMap;
@@ -10,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         BinaryTree root = initTree();
 
-        TreeMap<Integer, Integer> hashMap = new TreeMap<>();
+        TreeMap<Integer, Pair> hashMap = new TreeMap<>();
         TopView topView = new TopView(hashMap);
         topView.topView(root);
     }
@@ -20,21 +21,19 @@ public class Main {
     *       3
     *      /  \
     *     2     4
-    *    / \   / \
-    *   1   7 8   5
-    *              \
-    *               6
+    *    / \
+    *   1   7
+    *        \
+    *         10
     * */
     public static BinaryTree initTree(){
-        BinaryTree rightNode = new BinaryTree(6, null, null);
+        BinaryTree rightNode = new BinaryTree(10, null, null);
 
         BinaryTree level3First = new BinaryTree(1, null, null);
-        BinaryTree level3Second = new BinaryTree(7, null, null);
-        BinaryTree level3Third = new BinaryTree(8, null, null);
-        BinaryTree level4Fourth = new BinaryTree(5, null, rightNode);
+        BinaryTree level3Second = new BinaryTree(7, null, rightNode);
 
         BinaryTree level2First = new BinaryTree(2, level3First, level3Second);
-        BinaryTree level2Second = new BinaryTree(4, level3Third, level4Fourth);
+        BinaryTree level2Second = new BinaryTree(4, null, null);
 
         return new BinaryTree(3, level2First, level2Second);
     }
